4238817ffc7125e2d7a6806b542ec853ca3a378f
The404Hacking <the404hacking.team@gmail.com>
2017-10-17 23:46:45 +0330

    vrn
    👨 Code for "Large Pose 3D Face Reconstruction from a Single Image via
    Direct Volumetric CNN Regression" .

---
+++ vis.py
@@ -0,0 +1,45 @@
+import numpy as np
+import visvis as vv
+import argparse
+
+parser = argparse.ArgumentParser(description='Visualise the 3D volume')
+parser.add_argument('--image', dest='image',
+                    help="The background image to display")
+parser.add_argument('--volume', dest='volume',
+                    help="The volume to render")
+parser.add_argument('--texture', dest='texture',
+                    help="Show textured mesh NOT YET IMPLEMENTED")
+
+args = parser.parse_args()
+
+# Worth mentioning that the Z component (axis=0) is twice as deep as
+# the X and Y components are wide/tall. I'm going to leave these
+# unscaled for now, but if you know how to scale the mesh from
+# showvol, I would love to know.
+vol = np.fromfile(args.volume, dtype=np.int8)
+vol = vol.reshape((200,192,192))
+
+im = vv.imread(args.image)
+
+t = vv.imshow(im)
+t.interpolate = True # interpolate pixels
+
+# volshow will use volshow3 and rendering the isosurface if OpenGL
+# version is >= 2.0. Otherwise, it will show slices with bars that you
+# can move (much less useful).
+volRGB = np.stack(((vol > 1) * im[:,:,0],
+                   (vol > 1) * im[:,:,1],
+                   (vol > 1) * im[:,:,2]), axis=3)
+
+v = vv.volshow(volRGB, renderStyle='iso')
+
+l0 = vv.gca()
+l0.light0.ambient = 0.9 # 0.2 is default for light 0
+l0.light0.diffuse = 1.0 # 1.0 is default
+
+a = vv.gca()
+a.camera.fov = 0 # orthographic
+
+vv.use().Run()
+
+
